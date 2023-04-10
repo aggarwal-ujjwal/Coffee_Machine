14ce1f814a82a95eb6a0f90489b4b2e98adad525
aggarwal-ujjwal <aggarwal.ujjwal.17@gmail.com>
2023-04-10 23:50:08 +0530

    Stage 2 solution

--- Coffee Machine (Java)/task/src/machine/CoffeeMachine.java
+++ Coffee Machine (Java)/task/src/machine/CoffeeMachine.java
@@ -1,14 +1,24 @@
 package machine;
 
+import java.util.Scanner;
+
 public class CoffeeMachine {
     public static void main(String[] args) {
 
-        System.out.println("Starting to make a coffee\n" +
-                "Grinding coffee beans\n" +
-                "Boiling water\n" +
-                "Mixing boiled water with crushed coffee beans\n" +
-                "Pouring coffee into the cup\n" +
-                "Pouring some milk into the cup\n" +
-                "Coffee is ready!");
+//        System.out.println("Starting to make a coffee\n" +
+//                "Grinding coffee beans\n" +
+//                "Boiling water\n" +
+//                "Mixing boiled water with crushed coffee beans\n" +
+//                "Pouring coffee into the cup\n" +
+//                "Pouring some milk into the cup\n" +
+//                "Coffee is ready!");
+
+        System.out.println("Write how many cups of coffee you will need: ");
+        Scanner scanner = new Scanner(System.in);
+        int noOfCups = scanner.nextInt();
+        System.out.printf("For %d cups of coffee you will need:\n",noOfCups);
+        System.out.printf("%d ml of water\n", noOfCups*200);
+        System.out.printf("%d ml of milk\n", noOfCups*50);
+        System.out.printf("%d g of coffee beans\n", noOfCups*15);
     }
 }
